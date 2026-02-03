8c14834a161a8a1e2560094a0bd7abf3aeaeee09
andontakev <andontakev@gmail.com>
2026-02-03 20:49:19 +0200

    New things (it won't start)

--- PresAndoClothesShop/Controllers/CartController.cs
+++ PresAndoClothesShop/Controllers/CartController.cs
@@ -1,20 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PresAndoClothesShop.Data;
 using PresAndoClothesShop.Models;
 
 namespace PresAndoClothesShop.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ClothesShopContext _context;
         private readonly Cart _cart;
-        public CartController(Cart cart)
+        public CartController(ClothesShopContext context, Cart cart)
         {
             _cart = cart;
+            _context = context;
         }
         public IActionResult Index()
         {
             var items = _cart.GetAllCartItems();
             _cart.CartItems = items;
             return View(_cart);
+        }
+        public IActionResult AddToCart(int id)
+        { 
+            var selectedProduct = GetProductById(id);
+            if (selectedProduct != null)
+            {
+                _cart.AddToCart(selectedProduct, 1);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult RemoveFromCart(int id)
+        {
+            var selectedProduct = GetProductById(id);
+            if (selectedProduct != null)
+            {
+                _cart.RemoveFromCart(selectedProduct);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult IncreaseQuantity(int id)
+        {
+            var selectedProduct = GetProductById(id);
+            if (selectedProduct != null)
+            {
+                _cart.IncreaseQuantity(selectedProduct);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult ClearCart()
+        {
+            _cart.ClearCart();
+            return RedirectToAction("Index");
+        }
+        public Product GetProductById(int id)
+        { 
+            return _context.Products.FirstOrDefault(p => p.Id == id);
         }
     }
 }
--- PresAndoClothesShop/Models/Cart.cs
+++ PresAndoClothesShop/Models/Cart.cs
@@ -20,7 +20,77 @@ namespace PresAndoClothesShop.Models
             session.SetString("CartId", cartId);
             return new Cart(context) { Id = cartId };
         }
-
+        public CartItem GetCartItem(Product product)
+        {
+            return _context.CartItems
+                .SingleOrDefault(ci => ci.CartId == Id && ci.CartId == Id);
+        }
+        public void AddToCart(Product product, int quantity)
+        {
+            var cartItem = GetCartItem(product);
+            if (cartItem == null)
+            {
+                cartItem = new CartItem
+                {
+                    Product = product,
+                    Quantity = quantity,
+                    CartId = Id
+                };
+                _context.CartItems.Add(cartItem);
+            }
+            else
+            { 
+                cartItem.Quantity += quantity;
+            }
+            _context.SaveChanges();
+        }
+        public int ReduceQuantity(Product product)
+        {
+            var cartItem = GetCartItem(product);
+            int remainingQuantity = 0;
+            if (cartItem != null)
+            {
+                if (cartItem.Quantity > 1)
+                {
+                    cartItem.Quantity--;
+                    remainingQuantity = cartItem.Quantity;
+                }
+                else
+                {
+                    _context.CartItems.Remove(cartItem);
+                }
+            }
+            _context.SaveChanges();
+            return remainingQuantity;
+        }
+        public int IncreaseQuantity(Product product)
+        {
+            var cartItem = GetCartItem(product);
+            int newQuantity = 0;
+            if (cartItem != null)
+            {
+                cartItem.Quantity++;
+                newQuantity = cartItem.Quantity;
+            }
+            _context.SaveChanges();
+            return newQuantity;
+        }
+        public void RemoveFromCart(Product product)
+        {
+            var cartItem = GetCartItem(product);
+            if (cartItem != null)
+            {
+                _context.CartItems.Remove(cartItem);
+            }
+            _context.SaveChanges();
+        }
+        public void ClearCart()
+        {
+            var cartItems = _context.CartItems
+                .Where(ci => ci.CartId == Id);
+            _context.CartItems.RemoveRange(cartItems);
+            _context.SaveChanges();
+        }
         public List<CartItem> GetAllCartItems()
         {
             return new List<CartItem>(); /* CartItems ??
@@ -28,6 +98,13 @@ namespace PresAndoClothesShop.Models
                 .Where(ci => ci.CartId == Id)
                 .Include(ci => ci.Product)
                 .ToList());*/
+        }
+        public int GetCartTotal()
+        {
+            return _context.CartItems
+                .Where(ci => ci.CartId == Id)
+                .Select(ci => ci.Product.Price * ci.Quantity)
+                .Sum();
         }
     }
 }
