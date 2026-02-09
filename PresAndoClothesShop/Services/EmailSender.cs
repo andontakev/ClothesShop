0546810dd3165977d901de46bc548b8f59ba1f6f
andontakev <andontakev@gmail.com>
2026-02-10 00:23:55 +0200

    Remove SendGrid API key from history

--- PresAndoClothesShop/Services/EmailSender.cs
+++ PresAndoClothesShop/Services/EmailSender.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Net;
 using System.Net.Mail;
 
@@ -6,25 +8,30 @@ namespace PresAndoClothesShop.Services
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var fromEmail = "ourEmail@gmail.com";
-            var fromPassword = "ourPassword123!";
+            var apiKey = _config["SendGrid:ApiKey"];
+            var client = new SendGridClient(apiKey);
 
-            MailMessage message = new();
-            message.From = new MailAddress(fromEmail);
-            message.To.Add(new MailAddress(email));
-            message.Subject = subject;
-            message.Body = htmlMessage;
-            message.IsBodyHtml = true;
+            var from = new EmailAddress("andontakev@gmail.com", "PresAndo Shop");
+            var to = new EmailAddress(email);
 
-            var smtpClient = new SmtpClient("smtp.gmail.com")
-            {
-                Port = 587,
-                Credentials = new NetworkCredential(fromEmail, fromPassword),
-                EnableSsl = true
-            };
-            smtpClient.Send(message);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, null, htmlMessage);
+            var response = await client.SendEmailAsync(msg);
+            var body = await response.Body.ReadAsStringAsync();
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(body);
+
+            /*await client.SendEmailAsync(msg);*/
         }
     }
-} 
+
+}
+
